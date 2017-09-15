f89e2cc5b240c10aaa1bab2b88821147dd194fe3
robuntu <robuntu@localhost>
2017-09-15 14:27:59 -0400

    xddd added more words

--- src/HelloSync.java
+++ src/HelloSync.java
@@ -3,6 +3,9 @@ public class HelloSync {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Say Hello
+		System.out.println("I can't stop saying high xdd");
 		System.out.println("Hello from school");
 
 	}
